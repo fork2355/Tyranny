599726e9e18b49a0032b7481692bdc43ee714409
kings <kings@MSI>
2021-07-27 13:44:20 -0800

    Add project files.

---
+++ Tyranny/Program.cs
@@ -0,0 +1,21 @@
+﻿using Console = Colorful.Console;
+using System.Drawing;
+
+namespace Tyranny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                while(true)
+                {
+                    Console.Write("welcome to ", Color.White);
+                    Console.Write("Tyranny\n", Color.Red);
+                    Console.Write("the best community for retards\n", Color.Gray);
+                }
+            }
+        }
+    }
+}
